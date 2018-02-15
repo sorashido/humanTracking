03369fc07fa3e41bf30cb661af15f220d1252f20
shido <poporotylm@gmail.com>
2018-02-16 02:52:21 +0900

    example

--- modeling/rvo_sample/example.py
+++ modeling/rvo_sample/example.py
@@ -5,61 +5,62 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # timestep, neighborDist, maxNeighbors, timeHorizon, radius, maxSpeed, velocity
-sim = rvo2.PyRVOSimulator(1/60., 1.5, 5, 1.5, 2, 0.4, 2)
+sim = rvo2.PyRVOSimulator(1/5., 1.5, 5, 1.5, 2, 0.4, 10)
 
-# position, neighborDist, maxNeighbors, timeHorizon, timeHorizonObst, adius, maxSpeed, velocity
-a0 = sim.addAgent((-1, 1), 1.5, 5, 1.5, 2, 0.4, 2, (1, 0))
-a1 = sim.addAgent((-1, 0), 1.5, 5, 1.5, 2, 0.4, 2, (1, 0))
-a2 = sim.addAgent((-1, -1), 1.5, 5, 1.5, 2, 0.4, 2, (1, 0))
-
-a3 = sim.addAgent((1, 1), 1.5, 5, 1.5, 2, 0.4, 2, (-1, 0))
-a4 = sim.addAgent((1, 0), 1.5, 5, 1.5, 2, 0.4, 2, (-1, 0))
-a5 = sim.addAgent((1, -1), 1.5, 5, 1.5, 2, 0.4, 2, (-1, 0))
-
+# position, neighborDist, maxNeighbors, timeHorizon, timeHorizonObst, radius, maxSpeed, velocity
+# a0 = sim.addAgent((-1, 1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+# a1 = sim.addAgent((-1, 0), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+# a2 = sim.addAgent((-1, -1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+#
+# a3 = sim.addAgent((1, 1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+# a4 = sim.addAgent((1, 0), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+# a5 = sim.addAgent((1, -1), 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+#
 o1 = sim.addObstacle([(0.1, 0.1), (-0.1, 0.1), (-0.1, -0.1)])
 sim.processObstacles()
 
-sim.setAgentPrefVelocity(a0, (1, 0))
-sim.setAgentPrefVelocity(a1, (1, 0))
-sim.setAgentPrefVelocity(a2, (1, 0))
-sim.setAgentPrefVelocity(a3, (-1, 0))
-sim.setAgentPrefVelocity(a4, (-1, 0))
-sim.setAgentPrefVelocity(a5, (-1, 0))
+#
+# sim.setAgentPrefVelocity(a0, (1, 0))
+# sim.setAgentPrefVelocity(a1, (1, 0))
+# sim.setAgentPrefVelocity(a2, (1, 0))
+# sim.setAgentPrefVelocity(a3, (-1, 0))
+# sim.setAgentPrefVelocity(a4, (-1, 0))
+# sim.setAgentPrefVelocity(a5, (-1, 0))
 
+agents = []
+def addAgent(position, velocity):
+    a = sim.addAgent(position, 1.5, 5, 1.5, 2, 0.4, 2, (0, 0))
+    sim.setAgentPrefVelocity(a, velocity)
+    agents.append(a)
 
-print('Simulation has %i agents and %i obstacle vertices in it.' %
-      (sim.getNumAgents(), sim.getNumObstacleVertices()))
+for i in range(10):
+    addAgent((-10, 5 - i), (1, 0))
 
-print('Running simulation')
+for i in range(10):
+    addAgent((10, 5 - i), (-1, 0))
 
-fig = plt.figure()
-plt.xlim(-2, 2)
-plt.ylim(-2, 2)
-
+cmap=plt.get_cmap("tab20")
 def plot(data):
     sim.doStep()
     positions = ['(%5.3f, %5.3f)' % sim.getAgentPosition(agent_no)
-                 for agent_no in (a0, a1, a2, a3, a4, a5)]
+                 for agent_no in agents]
 
     print('t=%.3f  %s' % (sim.getGlobalTime(), '  '.join(positions)))
 
-    for agent_no in (a0, a1, a2, a3, a4, a5):
+    for agent_no in agents:
         x, y = sim.getAgentPosition(agent_no)
-        plt.plot(x, y, "ro")
+        plt.plot(x, y, color=cmap(agent_no), marker="*", markersize=3)
 
     for i in range(sim.getNumObstacleVertices()):
         x, y = sim.getObstacleVertex(i)
         plt.plot(x, y, "bo")
 
-ani = animation.FuncAnimation(fig, plot, interval=100)
+print('Simulation has %i agents and %i obstacle vertices in it.' %
+      (sim.getNumAgents(), sim.getNumObstacleVertices()))
+print('Running simulation')
+
+fig = plt.figure()
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+ani = animation.FuncAnimation(fig, plot, interval=10)
 plt.show()
-
-# for step in range(20):
-#     sim.doStep()
-#
-#     positions = ['(%5.3f, %5.3f)' % sim.getAgentPosition(agent_no)
-#                  for agent_no in (a0, a1, a2, a3)]
-#     print('step=%2i  t=%.3f  %s' % (step, sim.getGlobalTime(), '  '.join(positions)))
-
-# for i in range(20):
-#     plot("data")
